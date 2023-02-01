b0e43cddc7bcbe831317ee1bf01940252eb3e7d2
muhammad-asher <muhammadasher.se@gmail.com>
2023-02-01 17:28:36 +0500

    Complete functionality of app i.e templates,create models,migrations

--- products/views.py
+++ products/views.py
@@ -1,3 +1,20 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
+from .models import Offer
+
 
 # Create your views here.
+
+def index(request):
+    products = Product.objects.all()
+    return render(request, 'index.html', {'products': products})
+
+
+def show(request):
+    return HttpResponse("New Product")
+
+
+def offers(request):
+    offers = Offer.objects.all()
+    return render(request, 'offer.html', {'offers': offers})
