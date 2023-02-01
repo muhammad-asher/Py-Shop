8e9799c7f690cf925e8bc03e9504f14764f37ed7
muhammad-asher <muhammadasher.se@gmail.com>
2023-02-01 13:45:03 +0500

    secret key env fix

--- pyshop/settings.py
+++ pyshop/settings.py
@@ -19,8 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bzp3tkrx2obrhbnw7jxay2-48&1q1zt5f_^3%2m279tk$u&e11'
+# # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = 'django-insecure-bzp3tkrx2obrhbnw7jxay2-48&1q1zt5f_^3%2m279tk$u&e11'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
